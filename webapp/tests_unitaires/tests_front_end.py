9163e8408a7f4712077c7fb8c6401b8774515c55
Yannis Saliniere <yannis.saliniere@gmail.com>
2021-07-11 15:04:51 +0200

    version 0.0.5 branch 02_bug_clubs_using_more_points_allowed ajout LiveServerTestCase

--- webapp/tests_unitaires/tests_front_end.py
+++ webapp/tests_unitaires/tests_front_end.py
@@ -1,21 +1,32 @@
 import unittest
 import time
-from parameterized import parameterized
+from flask_testing import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import ui
+from parameterized import parameterized
+
+from .. import app
 
 
-class FrontEndBookingUnitTests(unittest.TestCase):
+class FrontEndBookingUnitTests(LiveServerTestCase):
+    """
+    class to test front end booking
+    """
+    def create_app(self):
+        app.config.from_object('webapp.tests_unitaires.config')
+        return app
 
     def setUp(self):
         self.driver = webdriver.Firefox()
+        self.wait = ui.WebDriverWait(self.driver, 1000)
 
     @staticmethod
     def pause():
         """
         Method pause to let the user see the page
         """
-        time.sleep(2)
+        time.sleep(app.config['IMPLICIT_WAIT'])
 
     @parameterized.expand([
         ("Fall Classic", "Iron Temple", "14"),
@@ -25,13 +36,12 @@ class FrontEndBookingUnitTests(unittest.TestCase):
         Front-End unit test in UI booking.html for case :
         places required greater than competition's number of places
         """
-        driver = self.driver
-        driver.get(f"http://127.0.0.1:5000/book/{competition_name}/{club_name}")
-        self.assertIn(f"Booking for {competition_name}", driver.title)
-        places_required = driver.find_element_by_id("places")
+        self.driver.get(self.get_server_url() + "/book/" + f"{competition_name}/{club_name}")
+        self.assertIn(f"Booking for {competition_name}", self.driver.title)
+        places_required = self.driver.find_element_by_id("places")
         places_required.send_keys(places)
         places_required.send_keys(Keys.RETURN)
-        assert "Number of places required is greater than competition's number of places" in driver.page_source
+        assert "Number of places required is greater than competition's number of places" in self.driver.page_source
         self.pause()
 
     @parameterized.expand([
@@ -42,13 +52,12 @@ class FrontEndBookingUnitTests(unittest.TestCase):
         Front-End unit test in UI booking.html for case :
         places required greater than club's points
         """
-        driver = self.driver
-        driver.get(f"http://127.0.0.1:5000/book/{competition_name}/{club_name}")
-        self.assertIn(f"Booking for {competition_name}", driver.title)
-        places_required = driver.find_element_by_id("places")
+        self.driver.get(self.get_server_url() + "/book/" + f"{competition_name}/{club_name}")
+        self.assertIn(f"Booking for {competition_name}", self.driver.title)
+        places_required = self.driver.find_element_by_id("places")
         places_required.send_keys(places)
         places_required.send_keys(Keys.RETURN)
-        assert "Number of places required is greater than club's points" in driver.page_source
+        assert "Number of places required is greater than club's points" in self.driver.page_source
         self.pause()
 
     def tearDown(self):
