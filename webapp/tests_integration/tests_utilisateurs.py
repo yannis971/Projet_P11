513598a93be1f9fede345185268925e8f74742a3
Yannis Saliniere <yannis.saliniere@gmail.com>
2021-07-13 23:49:31 +0200

    version 0.0.3 branch 05_bug_point_updates_not_deflected

--- webapp/tests_integration/tests_utilisateurs.py
+++ webapp/tests_integration/tests_utilisateurs.py
@@ -45,9 +45,7 @@ class UsersTests(LiveServerTestCase):
     def clicks_on_book(self, competition_name, club_name):
         links = self.driver.find_elements_by_tag_name("a")
         url = self.get_server_url() + "/book/" + f"{competition_name}/{club_name}"
-        print("url", url)
         for link in links:
-            print(link.get_attribute("href").replace("%20", " "))
             if link.get_attribute("href").replace("%20", " ") == url:
                 ActionChains(self.driver).click(link).perform()
                 break
